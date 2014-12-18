8e109941c956edd72c21d3e5d83ab53df6e76875
Michael Angerman <stormasm@gmail.com>
2014-12-18 14:58:28 -0800

    more queries

--- test/test01.rb
+++ test/test01.rb
@@ -2,10 +2,22 @@ require '../lib/client'
 require '../lib/request'
 
 @client = CensusApi::Client.new(ENV['CENSUS_API_KEY'], dataset: 'SF1')
-#data = @client.where({ fields: 'P0010001', level: 'COUNTY:001', within: 'STATE:06' })
-# new mexico
+
+# CSA New Mexico
 #data = @client.where({ fields: 'P0010001', level: 'CSA', within: 'STATE:35' })
-# oregon
+# CSA Oregon
 #data = @client.where({ fields: 'P0010001', level: 'CSA', within: 'STATE:41' })
-data = @client.where({ fields: 'P0010001', level: 'CSA', within: 'STATE:06' })
+
+# State
+#data = @client.where({ fields: 'P0010001', level: 'STATE' })
+
+# County
+#data = @client.where({ fields: 'P0010001', level: 'COUNTY' })
+# Counties New Mexico
+#data = @client.where({ fields: 'P0010001', level: 'COUNTY', within: 'STATE:35' })
+# Counties Oregon
+#data = @client.where({ fields: 'P0010001', level: 'COUNTY', within: 'STATE:41' })
+# Bernalillo County
+data = @client.where({ fields: 'P0010001', level: 'COUNTY:001', within: 'STATE:35' })
+
 puts data
