5659b6dc2aaaf5324febf5c7360c61c867cd0e2f
Michael Angerman <stormasm@gmail.com>
2014-12-18 13:57:03 -0800

    update with combined statistical area

--- test/test01.rb
+++ test/test01.rb
@@ -2,5 +2,10 @@ require '../lib/client'
 require '../lib/request'
 
 @client = CensusApi::Client.new(ENV['CENSUS_API_KEY'], dataset: 'SF1')
-data = @client.where({ fields: 'P0010001', level: 'COUNTY:001', within: 'STATE:06' })
+#data = @client.where({ fields: 'P0010001', level: 'COUNTY:001', within: 'STATE:06' })
+# new mexico
+#data = @client.where({ fields: 'P0010001', level: 'CSA', within: 'STATE:35' })
+# oregon
+#data = @client.where({ fields: 'P0010001', level: 'CSA', within: 'STATE:41' })
+data = @client.where({ fields: 'P0010001', level: 'CSA', within: 'STATE:06' })
 puts data
