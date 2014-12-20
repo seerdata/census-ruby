e5bb310655dabbefb7b3f53aa019fee9e1606d2b
Michael Angerman <stormasm@gmail.com>
2014-12-19 17:17:53 -0800

    we are now working on 2000 and 2010

--- test/test01.rb
+++ test/test01.rb
@@ -1,28 +1,64 @@
 require '../lib/client'
 require '../lib/request'
 
-@client = CensusApi::Client.new(ENV['CENSUS_API_KEY'], dataset: 'SF1')
+@client = CensusApi::Client.new(ENV['CENSUS_API_KEY'], {vintage: 2010, dataset: 'SF1'})
 
 # CSA New Mexico
-#data = @client.where({ fields: 'P0010001', level: 'CSA', within: 'STATE:35' })
+data = @client.where({ fields: 'P0010001', level: 'CSA', within: 'STATE:35' })
+puts data
 # CSA Oregon
-#data = @client.where({ fields: 'P0010001', level: 'CSA', within: 'STATE:41' })
+data = @client.where({ fields: 'P0010001', level: 'CSA', within: 'STATE:41' })
+puts data
 
 # State
-#data = @client.where({ fields: 'P0010001', level: 'STATE' })
+data = @client.where({ fields: 'P0010001', level: 'STATE' })
+puts data
 
 # County
-#data = @client.where({ fields: 'P0010001', level: 'COUNTY' })
+data = @client.where({ fields: 'P0010001', level: 'COUNTY' })
+#puts data
+
 # Counties New Mexico
-#data = @client.where({ fields: 'P0010001', level: 'COUNTY', within: 'STATE:35' })
+data = @client.where({ fields: 'P0010001', level: 'COUNTY', within: 'STATE:35' })
+puts data
+
 # Counties Oregon
-#data = @client.where({ fields: 'P0010001', level: 'COUNTY', within: 'STATE:41' })
+data = @client.where({ fields: 'P0010001', level: 'COUNTY', within: 'STATE:41' })
+puts data
+
 # Bernalillo County
-#data = @client.where({ fields: 'P0010001', level: 'COUNTY:001', within: 'STATE:35' })
+data = @client.where({ fields: 'P0010001', level: 'COUNTY:001', within: 'STATE:35' })
+puts data
 
 # Places in New Mexico
-#data = @client.where({ fields: 'P0010001', level: 'PLACE', within: 'STATE:35' })
-# Silver City in New Mexico
-data = @client.where({ fields: 'P0010001', level: 'PLACE:73260', within: 'STATE:35' })
+@client = CensusApi::Client.new(ENV['CENSUS_API_KEY'], {vintage: 2010, dataset: 'SF1'})
+data = @client.where({ fields: 'P0010001', level: 'PLACE', within: 'STATE:35' })
+puts data
+puts; puts; puts
 
+@client = CensusApi::Client.new(ENV['CENSUS_API_KEY'], {vintage: 2000, dataset: 'SF1'})
+data = @client.where({ fields: 'P001001', level: 'PLACE:73260', within: 'STATE:35' })
+puts data
+
+# Silver City in New Mexico
+@client = CensusApi::Client.new(ENV['CENSUS_API_KEY'], {vintage: 2010, dataset: 'SF1'})
+data = @client.where({ fields: 'P0010001', level: 'PLACE:73260', within: 'STATE:35' })
+puts data
+puts
+
+@client = CensusApi::Client.new(ENV['CENSUS_API_KEY'], {vintage: 2000, dataset: 'SF1'})
+data = @client.where({ fields: 'P001001', level: 'PLACE:70500', within: 'STATE:35' })
+puts data
+
+@client = CensusApi::Client.new(ENV['CENSUS_API_KEY'], {vintage: 2010, dataset: 'SF1'})
+data = @client.where({ fields: 'P0010001', level: 'PLACE:70500', within: 'STATE:35' })
+puts data
+puts
+
+@client = CensusApi::Client.new(ENV['CENSUS_API_KEY'], {vintage: 2000, dataset: 'SF1'})
+data = @client.where({ fields: 'P001001', level: 'PLACE:02000', within: 'STATE:35' })
+puts data
+
+@client = CensusApi::Client.new(ENV['CENSUS_API_KEY'], {vintage: 2010, dataset: 'SF1'})
+data = @client.where({ fields: 'P0010001', level: 'PLACE:02000', within: 'STATE:35' })
 puts data
